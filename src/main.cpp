dadd0386fa0bb78f051b0a235a31878ee2900e20
CassCives <cass-cives@hotmail.com>
2021-08-16 09:56:32 -0300

    Se agrego la lectura RFID y de limpiaron algunas funciones innecesarias

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <Wire.h>
 #include <Adafruit_MLX90614.h>
 #include <Servo.h>
+#include <MFRC522.h>
 
 
 ///cosas para decidir:
@@ -18,47 +19,73 @@ Adafruit_MLX90614 mlx = Adafruit_MLX90614();
 #define cerradura 5 //asignacion relay conectado a la cerradura electrica
 #define laser 6 //asignacion pin laser que indica visualmente la posicion del sensor de temperatura al usuario
 #define buzzer 7 //asignacion pin buzzer
-//pines del 8 al 12 rfid
+#define sensorObstaculo 8 //asignacion pin detector de obstaculos Lm393
+//pines rfid
+#define RST_PIN  5// Pin de reset
+#define SS_PIN  53// Pin de slave select
+//pines del 50 al 53 rfid
 //el sensor de temperatura MLX90614 esta en los pines 20(sda) y 21(scl) pero no hace falta asignarlo
-#define sensorObstaculo 13 //asignacion pin detector de obstaculos Lm393
-#define pulsador 42 //solo de test
 
 
 const int intervaloVerificacion = 2000; //asignacion tiempo de demora si hay verificacon fallida del rfid
-bool puedeEntrar = false; //variable verificacion entrada rfid
-bool saleAlguien = false; //variable salida
-bool temperaturaValida = false; //variable temperatura
+//bool puedeEntrar = false; //variable verificacion entrada rfid
+//bool saleAlguien = false; //variable salida
+//bool temperaturaValida = false; //variable temperatura
 Servo rociador; //objeto del tipo servo para poder manejarlo
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Objeto mfrc522 enviando pines de slave select y reset
 int angulo1 = 180; //variables para guardar la posicion del servo
 int angulo2 = 0;
+byte LecturaUID[4]; // Array para almacenar el UID leido
+byte Usuario1[4]= {0xCA, 0x27, 0x71, 0x7f}; // NUMERO DEL USUARIO 1 
+byte Usuario2[4]= {0x8C, 0xEB, 0xD4, 0x35}; // NUMERO DEL USUARIO 2 
 
 
 //declaracion de funciones
 //declaracion de funciones
-void enviarDatos(){
-  //enviar datos a base de datos 
+bool comparaUID(byte lectura[],byte usuario[])
+{
+  for (byte i=0; i < 4; i++){    // bucle recorre de a un byte por vez el UID
+  if(lectura[i] != usuario[i])        // si byte de UID leido es distinto a usuario
+    return false;          // retorna falso
   }
-
-bool verificacionRfid(){
-  //verificar credencial con la base de datos
-  return true;
-  }
-
-bool medicionTemperatura(){
-  Serial.print("Ambiente = ");
-  Serial.print(mlx.readAmbientTempC());
-  Serial.print("ºC\tObjeto = ");
-  Serial.print(mlx.readObjectTempC());
-  Serial.println("c");
-  delay(500);
-  if(mlx.readObjectTempC()<38){
-      return true;
-  }
-  return false;
+  return true;           // si los 4 bytes coinciden retorna verdadero
 }
 
-bool deteccionObstaculo(){//cuando el sensor de obstaculo detecta se pone en low y cuando no hay nada en high
-  if(digitalRead(sensorObstaculo)==LOW){
+bool verificacionRfid(){
+  if (!mfrc522.PICC_IsNewCardPresent()){ 
+    return false;
+  }
+  if (!mfrc522.PICC_ReadCardSerial()){
+    return false;
+  }
+  Serial.print("UID:");
+  for (byte i = 0; i < 4; i++) { // bucle recorre de a un byte por vez el UID
+    if (mfrc522.uid.uidByte[i] < 0x10){  // si el byte leido es menor a 0x10
+      Serial.print(" 0"); // imprime espacio en blanco y numero cero
+    }
+    else{ 
+      Serial.print(" ");  // imprime un espacio en blanco
+    }
+    Serial.print(mfrc522.uid.uidByte[i], HEX);    // imprime el byte del UID leido en hexadecimal
+    LecturaUID[i]=mfrc522.uid.uidByte[i];     // almacena en array el byte del UID leido      
+  }
+  if(comparaUID(LecturaUID, Usuario1)){    // llama a funcion comparaUID con Usuario1
+    Serial.println("\tVerificado");
+    return true;
+  }
+  else if(comparaUID(LecturaUID, Usuario2)){    // llama a funcion comparaUID con Usuario2
+    Serial.println("\tVerificado");
+    return true;
+  }
+  else{  
+    Serial.println("\tNo Verificado");
+    return false;
+  }
+  mfrc522.PICC_HaltA();  // detiene comunicacion con tarjeta
+}
+
+bool deteccionObstaculo(){
+  if(digitalRead(sensorObstaculo)==LOW){//cuando el sensor de obstaculo detecta se pone en low y cuando no hay nada en high
     return true;
   }
   return false;
@@ -76,47 +103,52 @@ void rociarAlcohol(){
     delay(15);
   }
 }
-
 //fin declaracion de funciones
+//fin declaracion de funciones
+
 void setup() {
   //asignacion pin modes
   pinMode(ledRojo, OUTPUT);
   pinMode(ledVerde, OUTPUT);
-  pinMode(pulsador, INPUT);
   rociador.attach(servo); //se relaciona la variable del servo con su pin
   //cerradura
   //laser
   pinMode(buzzer, OUTPUT);
   //rfid
+  SPI.begin();        // inicializa bus SPI
+  mfrc522.PCD_Init();     // inicializa modulo lector
   mlx.begin(); //iniciacion sensor de temperatura
   Serial.begin(9600);
 }
 
 void loop() {
-  puedeEntrar = verificacionRfid();
-  saleAlguien = deteccionObstaculo();
-  temperaturaValida = medicionTemperatura();
-  
- // if(digitalRead(pulsador)==HIGH){
-    if(temperaturaValida){
-      digitalWrite(ledVerde, HIGH);
+  if(deteccionObstaculo()){
+    Serial.println("Abrir puerta salida");
+  }
+  if(verificacionRfid()){
+    if(mlx.readObjectTempC()<38){
+      //suena el buzzer
+      digitalWrite(ledVerde, HIGH);//se prende el led verde
       delay(500);
-      rociarAlcohol();
-      delay(500);
-      //abrir cerradura
-      delay(3000);
-      digitalWrite(ledVerde, LOW);
+      //se muestr la temp
+      rociarAlcohol();//se rocia alcohol
+      //se abre la cerradura
+      digitalWrite(ledVerde, LOW);//se apaga el led verde
     }
-    else if(!temperaturaValida){
+    else if(mlx.readObjectTempC()>38){
+      //suena el buzzer
       digitalWrite(ledRojo, HIGH);
+      //se muestra la temp
       delay(2000);
       digitalWrite(ledRojo, LOW);
     }
-    if(saleAlguien){
-      Serial.println("Abrir puerta salida");
-    }
-    else{
-      Serial.println("No sale nadie");
-    }
-  //}
+  }
+  delay(5000);
 }
+
+
+//Serial.print("Ambiente = ");
+//Serial.print(mlx.readAmbientTempC());
+//Serial.print("ºC\tObjeto = ");
+//Serial.print(mlx.readObjectTempC());
+//Serial.println("c");
